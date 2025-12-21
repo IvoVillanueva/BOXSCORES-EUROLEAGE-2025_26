312f2b82efee93d8c992f11274b3a4dc6540bf53
IvoVillanueva <villanueva_i1@hotmail.com>
2025-12-21 23:16:54 +0100

    cambiar archivos de sitio

---
+++ scripts/get_rounds.R
@@ -0,0 +1,39 @@
+
+library(tidyverse)
+library(jsonlite)
+library(httr)
+library(httr2)
+
+
+ronda_fn <- function(ronda) {
+  df <- paste0("https://feeds.incrowdsports.com/provider/euroleague-feeds/v2/competitions/E/seasons/E2025/games?teamCode=&phaseTypeCode=RS&roundNumber=", ronda) %>%
+    httr::GET(query = list()) %>%
+    httr::content() %>%
+    purrr::pluck("data") %>%
+    dplyr::tibble(value = .) %>%
+    tidyr::unnest_wider(value) %>%
+    tidyr::unnest_wider(round) %>%
+    tidyr::unnest_wider(confirmedDate, names_sep = "_") %>%
+    transmute(jornada = round, gamecode = code, date = lubridate::with_tz(lubridate::ymd_hms(date), "Europe/Madrid"))
+}
+
+ronda_df <- map_df(1:38, ronda_fn)
+
+write_csv(ronda_df, here::here("data/gamecodes_euroleague_2025-26.csv"))
+
+
+ronda_eu <- function(ronda) {
+  df <- paste0("https://feeds.incrowdsports.com/provider/euroleague-feeds/v2/competitions/U/seasons/U2025/games?teamCode=&phaseTypeCode=RS&roundNumber=", ronda) %>%
+    httr::GET(query = list()) %>%
+    httr::content() %>%
+    purrr::pluck("data") %>%
+    dplyr::tibble(value = .) %>%
+    tidyr::unnest_wider(value) %>%
+    tidyr::unnest_wider(round) %>%
+    tidyr::unnest_wider(confirmedDate, names_sep = "_") %>%
+    transmute(jornada = round, gamecode = code, date = lubridate::with_tz(lubridate::ymd_hms(date), "Europe/Madrid"))
+}
+
+ronda_eu <- map_df(1:18, ronda_eu)
+
+write_csv(ronda_eu, here::here("data/gamecodes_eurocup_2025-26.csv"))
