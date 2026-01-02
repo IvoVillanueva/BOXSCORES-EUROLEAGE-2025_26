a605170c574ffb52dff45cd6ea1712766dcf25e8
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2026-01-02 23:26:17 +0100

    Comment out unused source script calls
    Comment out the source calls for europecup and championsleague scripts.

--- scripts/main.R
+++ scripts/main.R
@@ -1,4 +1,4 @@
 source("scripts/boxscores_euroleague.R")
 source("scripts/boxscores_eurocup.R")
-source("scripts/boxscores_europecup.R")
-source("scripts/boxscores_championsleague.R")
+#source("scripts/boxscores_europecup.R")
+#source("scripts/boxscores_championsleague.R")
